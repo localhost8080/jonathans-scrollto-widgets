95f6144d5282a42194b60f8c1b410ec375454965
jonathan <jonathan@adeogroup.co.uk>
2015-11-24 16:24:58 +0000

    mods

--- jonathans-scrollto-widgets.php
+++ jonathans-scrollto-widgets.php
@@ -1,8 +1,8 @@
 <?php
 
 /*
- * Plugin Name: Wordpress Scrollto Widget
- * Plugin URI: https://github.com/localhost8080/wordpress-scrollto-widgets
+ * Plugin Name: Jonathans Scrollto Widget
+ * Plugin URI: https://github.com/localhost8080/wjonathans-scrollto-widgets
  * Description: fixed position anchor links on side of page with animated scroll and drag and drop widget for page-builders
  * Version: 1.0
  * Author: jonathan
