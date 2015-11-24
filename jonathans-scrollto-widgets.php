eeaee682db4906d01813e7d79264697f88c84544
jonathan <jonathan@adeogroup.co.uk>
2015-11-24 17:03:51 +0000

    move to php 5.3 initiator

--- jonathans-scrollto-widgets.php
+++ jonathans-scrollto-widgets.php
@@ -63,5 +63,10 @@ class jonathans_scrollto_widget extends WP_Widget
         }
     }
 }
-add_action('widgets_init', create_function('', 'return register_widget("jonathans_scrollto_widget");'));
+
+add_action('widgets_init', function ()
+{
+    register_widget('jonathans_scrollto_widget');
+});
+
 add_action('siteorigin_panel_enqueue_admin_scripts', 'jonathans_scrollto_widget_scripts');
