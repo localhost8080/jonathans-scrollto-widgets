a88c3ea4d92c35cd5ab3e01237b00d8d0214a549
jonathan <jonathan@adeogroup.co.uk>
2015-11-24 16:21:30 +0000

    mods + gitignore

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # wordpress-scrollto-widgets
 fixed position anchor links on side of page with animated scroll and drag and drop widget for page-builders
+
+https://jonathansblog.co.uk
+https://github.com/localhost8080/wordpress-scrollto-widgets
---
+++ wordpress-scrollto-widgets.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * Plugin Name: Wordpress Scrollto Widget
+ * Plugin URI: https://github.com/localhost8080/wordpress-scrollto-widgets
+ * Description: fixed position anchor links on side of page with animated scroll and drag and drop widget for page-builders
+ * Version: 1.0
+ * Author: jonathan
+ * Author URI: https://jonathansblog.co.uk
+ * License: GPL2
+ */
+?>
