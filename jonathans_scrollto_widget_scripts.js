3b5a46822c14f758e7a8d424186fdc9690b99089
jonathan <jonathan@adeogroup.co.uk>
2015-11-24 17:39:24 +0000

    mods

--- jonathans_scrollto_widget_scripts.js
+++ jonathans_scrollto_widget_scripts.js
@@ -1,9 +1,12 @@
 // javascripts
+(function() {
+	$('body').append('<ul class="jonathans-scrollto-widget-holder">');
 
-$('body').append('<ul class="jonathans-scrollto-widget-holder">');
-
-$(".jonathans-scrollto-widget").each(function(index, elem){
-	elem.id = "scrollto" + index;
-	// add an index to the new list
-	$('.jonathans-scrollto-widget-holder').append('<li><a href="#scrollto' + index +'">here</a></li>');
+	$(".jonathans-scrollto-widget").each(
+			function(index, elem) {
+				elem.id = "scrollto" + index;
+				// add an index to the new list
+				$('.jonathans-scrollto-widget-holder').append(
+						'<li><a href="#scrollto' + index + '">here</a></li>');
+			});
 });
