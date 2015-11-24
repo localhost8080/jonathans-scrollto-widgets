143b957366464997aaf3ef1f1a2150ceeb5db592
jonathan <jonathan@adeogroup.co.uk>
2015-11-24 16:50:13 +0000

    test

--- jonathans-scrollto-widgets.php
+++ jonathans-scrollto-widgets.php
@@ -16,7 +16,7 @@ class jonathans_scrollto_widget extends WP_Widget
      */
     function __construct()
     {
-        parent::WP_Widget(false, $name = __('jonathans scrollto widget', 'jonathans_scrollto_widget'));
+        parent::WP_Widget(false, $name = __('jonathans scrollto widget', '__construct'));
     }
 
     /**
