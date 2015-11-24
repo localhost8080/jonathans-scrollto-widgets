fc2ac6c47e01875b5c9416ccf8c8f95d978823cd
jonathan <jonathan@adeogroup.co.uk>
2015-11-24 16:43:37 +0000

    mods

--- jonathans-scrollto-widgets.php
+++ jonathans-scrollto-widgets.php
@@ -9,33 +9,48 @@
  * Author URI: https://jonathansblog.co.uk
  * License: GPL2
  */
-
-
-class jonathans_scrollto_widget extends WP_Widget {
-
-    // constructor
-    function __construct()() {
-    /* ... */
-}
-
-// widget form creation
-function form($instance)
+class jonathans_scrollto_widget extends WP_Widget
 {
-    /* ... */
-}
 
-// widget update
-function update($new_instance, $old_instance)
-{
-    /* ... */
-}
+    /**
+     */
+    function __construct()
+    {
+        parent::WP_Widget(false, $name = __('jonathans scrollto widget', 'jonathans_scrollto_widget'));
+    }
 
-// widget display
-function widget($args, $instance)
-{
-    /* ... */
-}
-}
+    /**
+     *
+     * @param unknown $instance            
+     */
+    function form($instance)
+    {
+        // dont need to do anything here
+    }
 
-// register widget
+    /**
+     * * * @param unknown $new_instance * @param unknown $old_instance
+     */
+    function update($new_instance, $old_instance)
+    {
+        // dont have anything to update
+    }
+
+    /**
+     * * * @param unknown $args * @param unknown $instance
+     */
+    function widget($args, $instance)
+    {
+        extract($args);
+        $this_widget_id = $this->id;
+        echo $before_widget;
+        // Display the widget
+        echo '<div class="jonathans-scrollto-widget" id="' . $this_widget_id . '">';
+        echo '<img src="' . plugin_dir_url(__FILE__) . 'anchor.png' . '">';
+        // Check if title is set
+        
+        echo '</div>';
+        echo $after_widget;
+    }
+}
 add_action('widgets_init', create_function('', 'return register_widget("jonathans_scrollto_widget");'));
