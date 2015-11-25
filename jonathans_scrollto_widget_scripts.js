3218f1d302a6ed50cef1d3a191cb753f18f6f35d
jonathan <jonathan@adeogroup.co.uk>
2015-11-25 12:29:20 +0000

    js mods

--- jonathans_scrollto_widget_scripts.js
+++ jonathans_scrollto_widget_scripts.js
@@ -1,41 +1,36 @@
 // javascripts
-$(document)
-.ready(
+$(document).ready(
     function() {
-        $('body').append('<ul class="jonathans-scrollto-widget-holder">');
-
         if ($('.jonathans-scrollto-widget').length) {
+            $('body').append('<ul class="jonathans-scrollto-widget-holder">');
             $('.jonathans-scrollto-widget-holder').append(
                 '<li><a href="#'
                 + index
                 + '"><span class="fa fa-circle"></span><span class="title">Top</span></a></li>');
-        });
 
+            $(".jonathans-scrollto-widget").each(
+                function(index, elem) {
+                    elem.id = "scrollto" + index;
+                    $('.jonathans-scrollto-widget-holder').append(
+                        '<li><a href="#scrollto'
+                        + index
+                        + '"><span class="fa fa-circle"></span><span class="title">'
+                        + $(this).data('src')
+                        + '</span></a></li>');
+            });
 
-$(".jonathans-scrollto-widget")
-.each(
-    function(index, elem) {
-        elem.id = "scrollto" + index;
-        $('.jonathans-scrollto-widget-holder').append(
-            '<li><a href="#scrollto'
-            + index
-            + '"><span class="fa fa-circle"></span><span class="title">'
-            + $(this).data('src')
-            + '</span></a></li>');
-    });
+            $('a[href^="#"]').on('click', function(event) {
 
-$('a[href^="#"]').on('click', function(event) {
+                var target = $($(this).attr('href'));
 
-    var target = $($(this).attr('href'));
+                if (target.length) {
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop : target.offset().top
+                    }, 1000);
+                }
 
-    if (target.length) {
-        event.preventDefault();
-        $('html, body').animate({
-            scrollTop : target.offset().top
-        }, 1000);
-    }
-
-});
-}
+            });
+        }
 
 });
